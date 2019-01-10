30606adc7e0fd3ca7883d0a6a870288f18db8ff3
秦朝勇 <qinchy@msn.com>
2019-01-10 11:21:51 +0800

    first commit

--- src/main/java/com/qinchy/multidbdemo/domain/master/SaasUserInfo.java
+++ src/main/java/com/qinchy/multidbdemo/domain/master/SaasUserInfo.java
@@ -1,16 +1,32 @@
 package com.qinchy.multidbdemo.domain.master;
 
+import lombok.*;
+import lombok.extern.java.Log;
+
 import java.util.Date;
 
+@Data
+@Setter
+@Getter
+@Log
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class SaasUserInfo {
     private String userId;
 
+    @NonNull
     private String userName;
 
+    @NonNull
     private String password;
 
+    @NonNull
     private String branchId;
 
+    @NonNull
     private String branchName;
 
     private String notes;
@@ -20,76 +36,4 @@ public class SaasUserInfo {
     private Date lastUpdateTime;
 
     private String lastUpdateUser;
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId == null ? null : userId.trim();
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName == null ? null : userName.trim();
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password == null ? null : password.trim();
-    }
-
-    public String getBranchId() {
-        return branchId;
-    }
-
-    public void setBranchId(String branchId) {
-        this.branchId = branchId == null ? null : branchId.trim();
-    }
-
-    public String getBranchName() {
-        return branchName;
-    }
-
-    public void setBranchName(String branchName) {
-        this.branchName = branchName == null ? null : branchName.trim();
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes == null ? null : notes.trim();
-    }
-
-    public String getValidFlag() {
-        return validFlag;
-    }
-
-    public void setValidFlag(String validFlag) {
-        this.validFlag = validFlag == null ? null : validFlag.trim();
-    }
-
-    public Date getLastUpdateTime() {
-        return lastUpdateTime;
-    }
-
-    public void setLastUpdateTime(Date lastUpdateTime) {
-        this.lastUpdateTime = lastUpdateTime;
-    }
-
-    public String getLastUpdateUser() {
-        return lastUpdateUser;
-    }
-
-    public void setLastUpdateUser(String lastUpdateUser) {
-        this.lastUpdateUser = lastUpdateUser == null ? null : lastUpdateUser.trim();
-    }
 }
